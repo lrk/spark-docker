b4322de23094f0ee4e605d0341214a5a4d7e68d0
Lrk <sfxelrick@gmail.com>
2019-04-25 01:08:05 +0200

    Fix spark version and SPARK_HOME env var

--- build_images.sh
+++ build_images.sh
@@ -2,7 +2,9 @@
 set -e
 
 BASE_IMG_NAME=3lr1ck/spark
-TAG=2.4.1-hadoop2.7-jdk8
+TAG=2.4.2-hadoop2.7-jdk8
+
+docker build --target base -t $BASE_IMG_NAME-base:$TAG -f docker/Dockerfile docker
 
 docker build --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
 docker build --target worker -t $BASE_IMG_NAME-worker:$TAG -f docker/Dockerfile docker
