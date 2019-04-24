0ba02e15e08ea4bbcb0cca244f2e037246b7eced
Cengis K <sfxelrick@gmail.com>
2019-04-25 01:17:07 +0200

    First version (#1)
    * Use multi stage docker builds
    add master and worker image
    
    * Add travis build status

---
+++ build_images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+BASE_IMG_NAME=3lr1ck/spark
+TAG=2.4.2-hadoop2.7-jdk8
+
+docker build --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
+docker build --target worker -t $BASE_IMG_NAME-worker:$TAG -f docker/Dockerfile docker
