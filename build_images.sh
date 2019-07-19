8c1a6b7a4e457e1242fabc99e4627a06edc94951
cengisk <sfxelrick@gmail.com>
2019-07-19 15:08:16 +0200

    add build matrix

--- build_images.sh
+++ build_images.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+DOCKER_BUILD_ARGS=
 if [ ! ${SPARK_VERSION}]; then
   SPARK_VERSION=2.4.3
+  DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg SPARK_VERSION=${SPARK_VERSION}"
 fi
 
 if [ ! ${HADOOP_VERSION}]; then
   HADOOP_VERSION=2.7
+  DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg HADOOP_VERSION=${HADOOP_VERSION}"
 fi
 
 BASE_IMG_NAME=3lr1ck/spark
 TAG=${SPARK_VERSION}-hadoop${HADOOP_VERSION}-jdk8
 
-docker build --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
-docker build --target worker -t $BASE_IMG_NAME-worker:$TAG -f docker/Dockerfile docker
-docker build --target submit -t $BASE_IMG_NAME-submit:$TAG -f docker/Dockerfile docker
+echo "Build arguments: ${DOCKER_BUILD_ARGS}"
+
+docker build ${DOCKER_BUILD_ARGS} --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
+docker build ${DOCKER_BUILD_ARGS} --target worker -t $BASE_IMG_NAME-worker:$TAG -f docker/Dockerfile docker
+docker build ${DOCKER_BUILD_ARGS} --target submit -t $BASE_IMG_NAME-submit:$TAG -f docker/Dockerfile docker
