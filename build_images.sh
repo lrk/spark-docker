60c3cabcf5967206eab966cb3a49c875cf742cbe
cengisk <sfxelrick@gmail.com>
2019-07-19 15:04:14 +0200

    add build matrix

--- build_images.sh
+++ build_images.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+if [ ! ${SPARK_VERSION}]; then
+  SPARK_VERSION=2.4.3
+fi
+
+if [ ! ${HADOOP_VERSION}]; then
+  HADOOP_VERSION=2.7
+fi
+
 BASE_IMG_NAME=3lr1ck/spark
-TAG=2.4.3-hadoop2.7-jdk8
+TAG=${SPARK_VERSION}-hadoop${HADOOP_VERSION}-jdk8
 
 docker build --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
 docker build --target worker -t $BASE_IMG_NAME-worker:$TAG -f docker/Dockerfile docker
