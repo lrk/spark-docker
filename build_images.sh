f36bd9b4f48036864ed95c2cc8f472bb52c11845
cengisk <sfxelrick@gmail.com>
2019-07-19 15:13:36 +0200

    add build matrix

--- build_images.sh
+++ build_images.sh
@@ -2,19 +2,17 @@
 set -e
 
 DOCKER_BUILD_ARGS=
-if [ ! ${SPARK_VERSION} ]; then
+if [ -z "${SPARK_VERSION}" ]; then
   SPARK_VERSION=2.4.3
-  DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg SPARK_VERSION=${SPARK_VERSION}"
 fi
-
-if [ ! ${HADOOP_VERSION} ]; then
+if [ -z "${HADOOP_VERSION}" ]; then
   HADOOP_VERSION=2.7
-  DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg HADOOP_VERSION=${HADOOP_VERSION}"
 fi
 
 BASE_IMG_NAME=3lr1ck/spark
 TAG=${SPARK_VERSION}-hadoop${HADOOP_VERSION}-jdk8
 
+DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg SPARK_VERSION=${SPARK_VERSION} --build-arg HADOOP_VERSION=${HADOOP_VERSION}"
 echo "Build arguments: ${DOCKER_BUILD_ARGS}"
 
 docker build ${DOCKER_BUILD_ARGS} --target master -t $BASE_IMG_NAME-master:$TAG -f docker/Dockerfile docker
